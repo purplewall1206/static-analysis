259c088000fac9cbe4156b8292418ed0c4ce1682
purplewall1206 <wzc@smail.nju.edu.cn>
2022-01-05 21:29:24 +0800

    how to add clang plugin

---
+++ FindClassDecls/README.md
@@ -0,0 +1,1 @@
+https://freecompilercamp.org/clang-AST-basics/
---
+++ FindClassDecls/build/CMakeFiles/find-class-decls.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/find-class-decls.dir/FindClassDecls.cpp.o"
+  "bin/find-class-decls"
+  "bin/find-class-decls.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/find-class-decls.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
